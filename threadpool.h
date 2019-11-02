e4b828f851c9d74d96b44fe3487935f5242a6318
Charles Le <le.charles55@gmail.com>
2019-11-01 22:27:00 -0600

    better?

--- threadpool.h
+++ threadpool.h
@@ -23,7 +23,6 @@ typedef struct {
     pthread_cond_t get_cond = PTHREAD_COND_INITIALIZER;
     pthread_cond_t kill_cond = PTHREAD_COND_INITIALIZER;
     pthread_mutex_t kill_mutex = PTHREAD_MUTEX_INITIALIZER;
-    pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
     pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
     std::vector<pthread_t*> threads;
     ThreadPool_work_queue_t * tasks;
